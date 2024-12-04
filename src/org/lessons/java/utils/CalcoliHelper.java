91703f6a86be4275bbabd9474a07e7602ba8415c
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-12-04 23:21:46 +0100

    added static sum methods with overloading support

--- src/org/lessons/java/utils/CalcoliHelper.java
+++ src/org/lessons/java/utils/CalcoliHelper.java
@@ -2,4 +2,13 @@ package org.lessons.java.utils;
 
 public class CalcoliHelper {
 
+    //Applico l'overloading ovvero piu metodi con lo stesso nome
+    
+    public static int somma(int a, int b){
+        return a + b;
+    }
+
+    public static double somma(double a, double b){
+        return a + b;
+    }
 }
