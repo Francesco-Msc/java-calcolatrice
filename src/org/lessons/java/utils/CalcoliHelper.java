3fef62bc2acef279469cbb35b8a2b8ee0ac2e1cb
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-12-04 23:24:47 +0100

    added requested methods for arithmetic operations and utility functions

--- src/org/lessons/java/utils/CalcoliHelper.java
+++ src/org/lessons/java/utils/CalcoliHelper.java
@@ -3,12 +3,76 @@ package org.lessons.java.utils;
 public class CalcoliHelper {
 
     //Applico l'overloading ovvero piu metodi con lo stesso nome
-    
+
     public static int somma(int a, int b){
         return a + b;
     }
 
     public static double somma(double a, double b){
         return a + b;
+    }
+
+    public static int differenza (int a, int b){
+        return a - b;
+    }
+
+    public static double differenza (double a, double b){
+        return a - b;
+    }
+
+    public static int moltiplicazione (int a, int b){
+        return a * b;
+    }
+
+    public static double moltiplicazione (double a, double b){
+        return a * b;
+    }
+
+    public static int assoluto (int a){
+        if(a < 0){
+            return -a;
+        } else {
+            return a;
+        }
+    }
+
+    public static double assoluto (double a){
+        if(a < 0){
+            return -a;
+        } else {
+            return a;
+        }
+    }
+
+    public static int minimo (int a, int b){
+        if(a < b){
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static double minimo (double a, double b){
+        if(a < b){
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static int massimo (int a, int b){
+        if(a > b){
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static double massimo (double a, double b){
+        if(a > b){
+            return a;
+        } else {
+            return b;
+        }
     }
 }
